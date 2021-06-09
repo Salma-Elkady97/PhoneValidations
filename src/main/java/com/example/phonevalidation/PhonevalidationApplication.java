6f4d9b83986e104cf5675dc84a7bcd976bf43b2f
Salma Elkady <salmaelkady1997@gmail.com>
2021-06-09 20:29:25 +0200

    init repo

---
+++ src/main/java/com/example/phonevalidation/PhonevalidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.phonevalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonevalidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonevalidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phonevalidation/PhonevalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phonevalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonevalidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
